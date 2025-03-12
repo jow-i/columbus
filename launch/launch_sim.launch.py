04a930c0fc2ae8dfc66ce189a776a3cae22b7543
jowi <owuor.jesse21@students.dkut.ac.ke>
2025-03-12 16:43:02 +0300

    added twist_converter to convert twist messages to twistStamped, added joystick control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -25,10 +25,13 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                launch_arguments={'extra gazebo args': '--ros-args --params-file' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -39,10 +42,10 @@ def generate_launch_description():
 
 
 
-    diff_drive_spawner = Node(
+    mecanum_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diff_cont"],
+        arguments=["mecanum_cont"],
     )
 
     joint_broad_spawner = Node(
@@ -51,13 +54,11 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
-
-
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
+        mecanum_drive_spawner,
+        joint_broad_spawner,
     ])
