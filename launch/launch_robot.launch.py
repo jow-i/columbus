3e1d6edc4495d2677fa1c44522406f71b3f90ed9
jowi <owuor.jesse21@students.dkut.ac.ke>
2025-03-18 12:51:06 +0300

    added launch robot script

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -4,8 +4,11 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
 
@@ -31,21 +34,46 @@ def generate_launch_description():
     #             )])
     # )
     
+    # robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[controller_params_file]
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    
     mecanum_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["mecanum_cont"],
     )
 
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[mecanum_drive_spawner],
+        )
+    )
+    
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_broad"],
     )
 
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
     # Launch them all!
     return LaunchDescription([
         rsp,
-        mecanum_drive_spawner,
-        joint_broad_spawner,
-    ])
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
+])
